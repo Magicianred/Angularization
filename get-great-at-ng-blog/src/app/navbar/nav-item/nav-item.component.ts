716ac202eac055549e90996b28874669c3f04ecc
pjnalls <preston.james.nalls@gmail.com>
2021-02-21 03:34:09 -0600

    Write Navbar stories & pass light boolean value.

--- get-great-at-ng-blog/src/app/navbar/nav-item/nav-item.component.ts
+++ get-great-at-ng-blog/src/app/navbar/nav-item/nav-item.component.ts
@@ -6,21 +6,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./nav-item.component.scss']
 })
 export class NavItemComponent {
-  @Input()
-  light = false;
+  @Input() light = false;
+  @Input() backgroundColor?: string;
+  @Input() label = '';
 
-  @Input()
-  backgroundColor?: string;
-
-  @Input()
-  label = '💡 NavItem';
-
-  @Output()
-  onClick = new EventEmitter<Event>();
+  @Output() onClick = new EventEmitter<Event>();
 
   public get classes(): string[] {
     const mode = this.light ? 'nav-item--light' : 'nav-item--dark';
-    this.label = this.light ? '💡 NavItem' : '🌙 NavItem';
 
     return ['nav-item', mode];
   }
--- get-great-at-ng-blog/src/app/navbar/navbar.component.ts
+++ get-great-at-ng-blog/src/app/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NavItemComponent } from './nav-item/nav-item.component';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Input() light = false;
+  @Input() backgroundColor?: string;
+  @Input() navItems: string[] = [];
+
+  public get classes(): string[] {
+    const mode = this.light ? 'navbar--light' : 'navbar--dark';
+
+    return ['navbar', mode];
+  }
 
   constructor() { }
 
