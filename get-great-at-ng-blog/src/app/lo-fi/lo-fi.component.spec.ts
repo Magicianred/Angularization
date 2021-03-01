ba14916c7bf5d9697d9045b81f158d77186dd3b4
pjnalls <preston.james.nalls@gmail.com>
2021-03-01 13:04:41 -0600

    Implement fidelity selection feature.

---
+++ get-great-at-ng-blog/src/app/lo-fi/lo-fi.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoFiComponent } from './lo-fi.component';
+
+describe('LoFiComponent', () => {
+  let component: LoFiComponent;
+  let fixture: ComponentFixture<LoFiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoFiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoFiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- get-great-at-ng-blog/src/app/theme-change/theme-change.component.ts
+++ get-great-at-ng-blog/src/app/theme-change/theme-change.component.ts
@@ -1,11 +1,21 @@
 import { Input } from '@angular/core';
 import { FontSize } from '../px-footer/font-size/font-size.enum';
+import { Fidelity } from '../px-footer/quality/fidelity.enum';
 
 export class ThemeChangeComponent {
+  constructor() { 
+    this.show = this.fidelity === Fidelity.Normal || this.fidelity === Fidelity.High;
+  }
+
   @Input() light = false;
   @Input() fontSize: FontSize = FontSize.Normal;
+  @Input() fidelity: Fidelity = Fidelity.Normal;
+  show: boolean;
 
   changeTheme(light: boolean) { this.light = light; }
-
   changeFontSize(size: FontSize) { this.fontSize = size; }
+  changeFidelity(fi: Fidelity) { 
+    this.fidelity = fi;
+    this.show = this.fidelity === Fidelity.Normal || this.fidelity === Fidelity.High; 
+  }
 }
